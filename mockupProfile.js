91d529b1d382705fcf2371674c8528fa94e707b1
LucianaMSanchez <lulutcht@hotmail.com>
2023-11-23 15:33:36 -0300

    json mockup

---
+++ mockupProfile.js
@@ -0,0 +1,21 @@
+/* entrada de datos */ 
+
+{
+    name: "nombre";
+    email: "email";
+    age: "edad";
+    gender: "genero";
+    weight: "peso";
+    height: "altura";
+    diabetesType: "tipo";
+    insuline1: {
+        name: "insulina";
+        basalDose: "dosis"
+    };
+    insuline2: {
+        name: "insulina";
+        basalDose: "dosis"
+    };
+    correctionFactor: "autofill";
+    bmi: "IMC autofill"
+}
